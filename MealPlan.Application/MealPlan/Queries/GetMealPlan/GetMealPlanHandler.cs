24a59215b5bce7e8f8694e214f5b8eebf41c28fd
Dean Dashwood <dean@dashwood.world>
2023-07-05 09:22:16 +0100

    Fill in blanks in meal plan

--- MealPlan.Application/MealPlan/Queries/GetMealPlan/GetMealPlanHandler.cs
+++ MealPlan.Application/MealPlan/Queries/GetMealPlan/GetMealPlanHandler.cs
@@ -16,12 +16,30 @@ internal class GetMealPlanHandler : IRequestHandler<GetMealPlanRequest, IEnumera
 
     public async Task<IEnumerable<MealPlanEntry>> Handle(GetMealPlanRequest request, CancellationToken cancellationToken)
     {
-        var results = await _context.MealPlanEntries
+        var existingEntries = await _context.MealPlanEntries
                         .Where(e => e.Date >= request.StartDate && e.Date <= request.EndDate)
                         .Include(e => e.People)
                         .Include(e => e.Location)
+                        .OrderBy(e => e.Date)
                         .AsSplitQuery()
                         .ToListAsync();
+
+        var results = new List<MealPlanEntry>();
+
+        var existingEnumerator = existingEntries.GetEnumerator();
+        existingEnumerator.MoveNext();
+        for (DateOnly date = request.StartDate; date <= request.EndDate; date = date.AddDays(1))
+        {
+            if (existingEnumerator.Current?.Date == date)
+            {
+                results.Add(existingEnumerator.Current);
+                existingEnumerator.MoveNext();
+            }
+            else
+            {
+                results.Add(new MealPlanEntry { Date = date });
+            }
+        }
 
         return results;
     }
