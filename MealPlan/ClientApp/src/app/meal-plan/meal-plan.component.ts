83f906ef20f65e9c128f8a5fd66a81f8688a8a7e
Dean Dashwood <dean@dashwood.world>
2023-07-05 13:40:52 +0100

    Implement infinite updating of the meal plan

--- MealPlan/ClientApp/src/app/meal-plan/meal-plan.component.ts
+++ MealPlan/ClientApp/src/app/meal-plan/meal-plan.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MealPlanService } from 'src/libs/api-client';
+import { Observable, Subject, map, merge, tap } from 'rxjs';
+import { MealPlanDto, MealPlanService } from 'src/libs/api-client';
 
 @Component({
   selector: 'app-meal-plan',
@@ -7,15 +8,18 @@ import { MealPlanService } from 'src/libs/api-client';
   styleUrls: ['./meal-plan.component.css']
 })
 export class MealPlanComponent implements OnInit {
+  mealPlanEntries!: MealPlanDto[];
+  private startDate: Date;
+  private endDate: Date;
+
   constructor (private mealPlanService : MealPlanService) {
+    this.startDate = new Date();
+    this.endDate = this.addDays(this.startDate, 7);
   }
+
   ngOnInit(): void {
-    var today = new Date();
-    var end = this.addDays(today, 7); // Start off with 1 week's data
-    this.mealPlanService.apiMealPlanGet(this.dateToString(today), this.dateToString(end))
-      .subscribe((meals) => {
-        
-      });
+    this.mealPlanService.apiMealPlanGet(this.dateToString(this.startDate), this.dateToString(this.endDate))
+        .subscribe(data => this.mealPlanEntries = data);
   }
 
   private addDays(date: Date, days: number) {
@@ -26,5 +30,13 @@ export class MealPlanComponent implements OnInit {
 
   private dateToString(date: Date) : string {
     return date.toISOString().split('T')[0];
+  }
+
+  public onLoadMore() {
+    let newStartDate = this.addDays(this.endDate, 1);
+    this.endDate = this.addDays(this.endDate, 7);
+
+    this.mealPlanService.apiMealPlanGet(this.dateToString(newStartDate), this.dateToString(this.endDate))
+        .subscribe(data => this.mealPlanEntries.push(...data));
   }
 }
