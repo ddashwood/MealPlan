ecb12f5a76f759702bc0563c40445fc908adf96e
Dean Dashwood <dean@dashwood.world>
2023-07-05 16:34:59 +0100

    Add tooltips

--- MealPlan/ClientApp/src/app/meal-plan-entry/meal-plan-entry.component.ts
+++ MealPlan/ClientApp/src/app/meal-plan-entry/meal-plan-entry.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MealPlanDto } from 'src/libs/api-client';
+declare var $:any;
 
 @Component({
   selector: 'app-meal-plan-entry',
   templateUrl: './meal-plan-entry.component.html',
   styleUrls: ['./meal-plan-entry.component.css']
 })
-export class MealPlanEntryComponent {
+export class MealPlanEntryComponent implements OnInit {
   @Input() entry!: MealPlanDto;
+
+  ngOnInit(): void {
+    $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  }
 }
