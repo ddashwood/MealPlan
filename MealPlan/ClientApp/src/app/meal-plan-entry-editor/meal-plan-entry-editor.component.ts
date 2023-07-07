5f76c974cbcefd6e26fc92e4a9a17e6bd411beca
Dean Dashwood <dean@dashwood.world>
2023-07-07 10:55:27 +0100

    Pass meal details into the editor

--- MealPlan/ClientApp/src/app/meal-plan-entry-editor/meal-plan-entry-editor.component.ts
+++ MealPlan/ClientApp/src/app/meal-plan-entry-editor/meal-plan-entry-editor.component.ts
@@ -1,12 +1,20 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { MealPlanDto } from 'src/libs/api-client';
 
 @Component({
   selector: 'app-meal-plan-entry-editor',
   templateUrl: './meal-plan-entry-editor.component.html',
   styleUrls: ['./meal-plan-entry-editor.component.css']
 })
-export class MealPlanEntryEditorComponent {
+export class MealPlanEntryEditorComponent implements OnInit {
   @Output() close = new EventEmitter<null>();
+  @Input() entry?: MealPlanDto;
+
+  ngOnInit(): void {
+    if (!this.entry) {
+      this.entry = <MealPlanDto> { };
+    }
+  }
 
   public onClose() {
     this.close.emit();
