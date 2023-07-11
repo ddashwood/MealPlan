a0b2f5939f3a7153b053b56a487e46919575125d
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2023-07-11 20:24:28 +0100

    Further improvements to home page

--- MealPlan/ClientApp/src/app/home/home.component.ts
+++ MealPlan/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { JWTTokenService } from '../services/jwt-token-service/jwttoken.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public loggedIn: boolean;
+
+  constructor (authService: JWTTokenService) {
+    this.loggedIn = !!authService.jwtToken;
+    authService.updated$.subscribe(() => this.loggedIn = !!authService.jwtToken);
+  }
 }
