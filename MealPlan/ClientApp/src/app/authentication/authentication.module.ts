699e333507cc7d98c29017cf77750fa0e7875e48
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2023-07-19 16:37:59 +0100

    Login and change password now use Material

--- MealPlan/ClientApp/src/app/authentication/authentication.module.ts
+++ MealPlan/ClientApp/src/app/authentication/authentication.module.ts
@@ -5,10 +5,14 @@ import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,12 @@ import { MatButtonModule } from '@angular/material/button';
     AuthenticationRoutingModule,
 
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+
+    FlexLayoutModule
   ]
 })
 export class AuthenticationModule { }
