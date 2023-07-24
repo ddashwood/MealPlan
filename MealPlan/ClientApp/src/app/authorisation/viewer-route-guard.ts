555968c27b4385f40e6dd2b94dadbacfb83247a7
Dean Dashwood <dean@dashwood.world>
2023-07-24 17:25:19 +0100

    Update to latest versions

--- MealPlan/ClientApp/src/app/authorisation/viewer-route-guard.ts
+++ MealPlan/ClientApp/src/app/authorisation/viewer-route-guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { JWTTokenService } from '../services/jwt-token-service/jwttoken.service';
 
 @Injectable()
-export class ViewerRouteGuard implements CanActivate {
+export class ViewerRouteGuard  {
     constructor(private tokenService: JWTTokenService) {
     }
 
